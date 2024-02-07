8766446d6b5def21798a874ad21cc9ca791ecdc0
Asis <asismelgarejo@gmail.com>
2024-02-07 08:03:24 -0500

    mongodb docker port was changed

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export async function InitializeDB(): Promise<typeof mongoose> {
   const dbStrConnection = process.env.MONGODB_STR_CONN || "";
-  const dbClient = await mongoose.connect(dbStrConnection, {});
+  const dbClient = await mongoose.connect(dbStrConnection, {
+    family: 4,
+  });
   console.log("Connected to Mongo");
   return dbClient;
 }
